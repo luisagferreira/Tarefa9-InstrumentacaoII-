c0262a821daca546a167d20c66150a0926672dd5
luisagferreira <62904118+luisagferreira@users.noreply.github.com>
2021-07-14 12:27:15 -0300

    Add files via upload

--- tarefa10_B.py
+++ tarefa10_B.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 n=np.arange(51)
 y=np.cos(0.05*np.pi*n)
 array1=np.array(y)
-print(np.std(array1))
-noise = np.random.normal(0.134,0.694,51)
+noise = np.random.normal(np.mean(array1),np.std(array1),51)
 ruido=array1+noise
 plt.figure(figsize=(10, 6), dpi=80)
 plt.title("Sinal com e sem ruído")
@@ -16,3 +15,4 @@ plt.plot(array1,label="Sinal Original")
 plt.plot(ruido,label="Sinal com ruído")
 plt.legend(loc=3)
 plt.show()
+
